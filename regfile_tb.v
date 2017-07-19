f67144110afb80c7348da4e1c2bcd6e6d80632c8
Matthew Otto <saturnstethys@gmail.com>
2017-07-19 17:12:15 -0400

    working test bench

--- regfile_tb.v
+++ regfile_tb.v
@@ -1,3 +1,4 @@
+//
 // Name: Matthew Otto
 // Last Modified: 2017/07/19
 //
@@ -5,17 +6,22 @@
 // Assignemnt: Lab8- Regfile testbench
 //
 module regfile_tb;
-reg [4:0] wInput1;             // machine inputs 
+reg [4:0] wInput1;               // machine inputs 
 reg [4:0] wInput2;
 reg [4:0] wWriteReg;
 reg [31:0] wWriteData;
-reg wClk,wWriteEN;
-wire [31:0] wData1;            // machine output
+reg wClk;
+reg wWriteEN;
+
+wire [31:0] wData1;             // machine outputs
 wire [31:0] wData2;
-reg [90:0] testvectors[0:100]; // [90:0] needs to be changed to [X:0] as X= truthTableLength-1 (DONE)
-reg [10:0] errors;             // counts how many rows were incorrect
-reg [10:0] vectornum;          // loop counterrows were incorrect
-reg [31:0] rightPC;            // truth table expected output
+
+reg [31:0] rightData1;          // correct outputs output
+reg [31:0] rightData2;
+
+reg [112:0] testvectors[0:112];  // [112:0] needs to be changed to [X:0] as X= truthTableLength-1 (DONE)
+reg [10:0] errors;               // counts how many rows were incorrect
+reg [10:0] vectornum;            // loop counterrows were incorrect
 
 // Connect UUT to test bench signals
 regfile uut( // sets 2 paramaters in order
@@ -36,7 +42,7 @@ initial  begin
 end 
 
 initial begin
-	$readmemh("testvec_lab8_regfile.txt", testvectors); //testvec is in hex
+	$readmemb("testvec_lab8_regfile.txt", testvectors); //testvec is in binary
 	vectornum = 0;
 	errors = 0;
 end
@@ -46,15 +52,26 @@ always begin
 		$display("%1d tests completed with %1d errors.", vectornum, errors);
 		$finish;
 	end
-	wClk = 0;
-	{wReset, wNextPC, rightPC} = testvectors[vectornum];
-	wClk = 1;
+	//wClk = 0;
+	// input1_input2_writeReg_writeData_writeEN__rightData1_rightData2_clk
+	{wInput1, wInput2, wWriteReg, wWriteData, wWriteEN, rightData1, rightData2, wClk} = testvectors[vectornum];
+	//wClk = 1;
 	#1  // must allow state machine time to run its behavioral code
-	if (rightPC !== wPC) begin 
-		errors = errors+1;	// found incorrect output
-		$display("Reset:%b NextPC:%h", wReset, wNextPC);
-		$display("Incorrectly outputs Y=%h expected:%h \n",wPC, rightPC);
+	
+	if (wData1 !== rightData1 || wData2 !== rightData2) begin 
+		$display("\nClk:%b WriteEN:%b Input1:%h Input2:%h WriteReg:%h WriteData:%h", wClk, wWriteEN, wInput1, wInput2, wWriteReg, wWriteData);
+		
+		if (wData1 !== rightData1) begin
+			$display("Incorrectly outputs Data1:%h expected:%h",wData1, rightData1);
+			errors = errors+1;	// found incorrect output
+		end
+		
+		if (wData2 !== rightData2) begin
+			$display("Incorrectly outputs Data2:%h expected:%h",wData2, rightData2);
+			errors = errors+1;	// found incorrect output
+		end
 	end
+	
 	vectornum = vectornum+1; // increments loop counter
 end
 endmodule
